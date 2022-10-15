670883a4f7173efee5067daf4ffa4656433ec103
massivedesigns <araluonyedika@gmail.com>
2022-10-14 19:42:07 -0700

    alphabet iteration in reverse order with for loop

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in reverse order followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar("\n");
+
+	return (0);
+}
