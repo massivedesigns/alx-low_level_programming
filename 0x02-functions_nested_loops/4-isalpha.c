001a0e4a5e8cf1cacd25403a50dfb216f47a82f6
massivedesigns <araluonyedika@gmail.com>
2022-10-18 20:21:41 -0700

    recoded

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,9 @@
 /**
  * _isalpha - checks for alphabet
  * @c: character to be checked
- * Return: 1 if character is a letter, 0 otherwise
-*/
-int _isalpha(c)
+ * Return: 1 if character is a letter,0 otherwise
+ */
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
