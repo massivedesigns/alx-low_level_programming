e108144bd007212159aa6a338ebf33a1f4b249d9
massivedesigns <araluonyedika@gmail.com>
2022-10-20 18:32:54 -0700

    another loops and nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints line on terminal using '_'
+ * @n: the number of times '_' is to be printed
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+	_putchare('\n');
+}
